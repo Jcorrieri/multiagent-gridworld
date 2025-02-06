13765538285b7b8224ed56a00bf9caed6416b220
Jcorrieri <jacomocorrieri@gmail.com>
2025-02-05 23:36:16 -0500

    edited ppo

--- models/ppo.py
+++ models/ppo.py
@@ -3,59 +3,68 @@ from argparse import Namespace
 import torch
 import torch.nn as nn
 from gymnasium import spaces
-from stable_baselines3 import PPO, DQN
+from stable_baselines3 import PPO
 from stable_baselines3.common.logger import Logger
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
 
 class CustomPPOExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space: spaces.Dict, features_dim: int = 64):
+    def __init__(self, observation_space: spaces.Dict, grid_size: int, features_dim: int = 64):
         super().__init__(observation_space, features_dim)
-
-        grid_shape = observation_space['map'].shape
-        self.n = grid_shape[0]
-        num_agents = observation_space['agents'].shape[0]
+        self.grid_size = grid_size
+        self.kernel_size = 5
+        self.stride = 1
 
         self.cnn = nn.Sequential(
-            nn.Conv2d(2, self.n, kernel_size=5, stride=1),
+            nn.Conv2d(
+                in_channels=2,
+                out_channels=grid_size,
+                kernel_size=self.kernel_size,
+                stride=self.stride
+            ),
             nn.ReLU(),
-            nn.Conv2d(self.n, self.n, kernel_size=5, stride=1),
+            nn.Conv2d(
+                in_channels=grid_size,
+                out_channels=grid_size,
+                kernel_size=self.kernel_size,
+                stride=self.stride
+            ),
             nn.ReLU(),
             nn.Flatten()
         )
 
-        test_input = torch.zeros(1, 2, self.n, self.n)
-        cnn_output_dim = self.cnn(test_input).view(1, -1).size(1)
+        def convert_to_size(size, kernel_size=5, stride=1):
+            return (size - (kernel_size - 1) - 1) // stride + 1
 
-        self.agent_fc = nn.Sequential(
-            nn.Linear(num_agents * 2, 32),
-            nn.ReLU()
-        )
+        self.width = convert_to_size(convert_to_size(grid_size))
+        self.height = convert_to_size(convert_to_size(grid_size))
 
-        self.fc = nn.Sequential(
-            nn.Linear(cnn_output_dim + 32, 256),
+        self.linear = nn.Sequential(
+            nn.Linear(grid_size * self.width * self.height, 256),
             nn.ReLU(),
-            nn.Linear(256, features_dim)
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, features_dim)
         )
 
-    def forward(self, observations: dict) -> torch.Tensor:
-        grid_map = observations['map'].float().unsqueeze(1)
-        robot_positions = observations['agents'].float()
+    def create_agent_channel(self, robot_positions, batch_size):
+        agent_channel = torch.zeros((batch_size, self.grid_size, self.grid_size),
+                                    device=robot_positions.device)
+        for b in range(batch_size):
+            pos = robot_positions[b]  # Shape: (n_robots, 2)
+            agent_channel[b, pos[:, 0].long(), pos[:, 1].long()] = 1
 
-        robot_positions_map = torch.zeros_like(grid_map)
-        for idx, agent_pos in enumerate(robot_positions):
-            x, y = agent_pos.int()
-            robot_positions_map[0, 0, x, y] = 1
+        return agent_channel
 
-        combined_map = torch.cat([grid_map, robot_positions_map], dim=1)
-        cnn_features = self.cnn(combined_map)
-        agent_locations = observations['agents'].float().view(observations['agents'].size(0), -1)
-        agent_features = self.agent_fc(agent_locations)
-        combined_features = torch.cat([cnn_features, agent_features], dim=1)
+    def forward(self, observations):
+        batch_size = observations['map'].shape[0]
+        grid_channel = observations['map'].float()
+        agent_channel = self.create_agent_channel(observations['agents'], batch_size)
+        x = torch.stack([grid_channel, agent_channel], dim=1)
+        cnn_features = self.cnn(x)
+        features = self.linear(cnn_features)
 
-        final_features = self.fc(combined_features)
-
-        return final_features
+        return features
 
 
 class CustomPPO(nn.Module):
@@ -67,6 +76,7 @@ class CustomPPO(nn.Module):
         else:
             policy_kwargs = dict(
                 features_extractor_class=CustomPPOExtractor,
+                features_extractor_kwargs=dict(grid_size=args.size,features_dim=64),
             )
             self.model = PPO("MultiInputPolicy", self.args.env, policy_kwargs=policy_kwargs, verbose=0,
                              learning_rate=3e-4,
@@ -89,6 +99,3 @@ class CustomPPO(nn.Module):
         self.model.set_logger(logger)
         self.model.learn(total_timesteps=total_timesteps)
         self.model.save("models/saved/Custom_PPO")
-
-    # def set_env(self, env):
-    #     self.model.set_env(env)
