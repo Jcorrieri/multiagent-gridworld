f08babc8ebbae212831ff4a6c024f71a48b9728f
Jcorrieri <jacomocorrieri@gmail.com>
2025-06-29 13:47:28 -0400

    rewrite README and rename models

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Multi-Robot Exploration with RLlib
 
+---
+
 ## Installation (Python v3.10)
 
 **Install Pytorch with CUDA support (Required)**
@@ -12,6 +14,8 @@ pip3 install torch torchvision torchaudio --index-url https://download.pytorch.o
 pip install ray[tune]==2.44.1 PettingZoo==1.22.3 dm_tree scipy pygame matplotlib lz4 pyyaml
 ```
 
+---
+
 ## Training Models
 
 **General Command:**
@@ -21,25 +25,33 @@ python main.py --config <file_name> --model_name <model_name> --seed <seed>
 
 **Example:**
 ```
-python main.py --config default --model_name mppo_connect_v1 --seed 42
+python main.py --config default --model_name model_v1 --seed 42
 ```
+
+---
 
 ## Testing Models
 
+The following arguments are optional arguments: 
+- --seed \<integer\> (defaults to 42)
+- --num_test_episodes \<integer\> (defaults to 1) 
+
 **General Command:**
 ```
-python main.py --test --config <file_name> --model_name <model_name> --seed <seed>
+python main.py --test --config <file_name> --model_name <model_name>
 ```
 
 **Full Connectivity Example:**
 ```
-python main.py --test --config rw5 --model_name mppo_connect_rw5 --seed 99
+python main.py --test --config s12_br10 --model_name s12_br10_d --seed 25 
 ```
 
 **Testing Model Checkpoints**
 ```
-python main.py --test --config rw5 --model_name ../ckpt/mppo_connect_rw5/3 --seed 100
+python main.py --test --config rw5 --model_name ../ckpt/model_name/3 
 ```
+
+---
 
 ## Project Structure and Configuration
 
@@ -70,3 +82,35 @@ python main.py --test --config rw5 --model_name ../ckpt/mppo_connect_rw5/3 --see
 - Add new obstacle layouts under env/obstacle_mats/.
 - Add custom configuration files under config/ and use --config <file_name> when running.
 - Models are saved or checkpointed under models/saved/ and models/ckpt/, respectively.
+- Metrics will be stored at the root '/' after training completes.
+
+---
+
+## Example configuration file format: 
+```yaml
+environment:
+    map_name: mat1
+    max_steps: 1000
+    size: 12
+    num_agents: 3
+    cr: 5
+
+reward_scheme:
+    new_tile_connected: 2.0
+    new_tile_disconnected: -10.0
+    old_tile_connected: -0.1
+    old_tile_disconnected: -10.0
+    obstacle: -1.0
+    terminated: 50
+
+training:
+    gamma: 0.9
+    lr: 0.0001
+    grad_clip: 1.0
+    train_batch_size: 2000
+    num_epochs: 5
+    minibatch_size: 200
+    l2_regularization: 0.0001
+    num_iterations: 8000
+    target_reward: 380
+```
--- main.py
+++ main.py
@@ -98,8 +98,6 @@ def test_one_episode(test_env: ParallelPettingZooEnv, model: Algorithm):
     return total_reward, steps, num_breaks
 
 def test(args, env_config) -> None:
-    game_env = ParallelPettingZooEnv(GridWorldEnv(**env_config))
-
     checkpoint_dir = os.path.abspath(f"models/saved/{args.model_name}")
 
     ModelCatalog.register_custom_model("shared_cnn", CustomTorchModelV2)
@@ -120,32 +118,36 @@ def test(args, env_config) -> None:
         print(f"| {'Percentage Connected:':<20} {round(100 * (1 - (brk / stp)), 2):>13}% |")
         print("-"*40)
 
-    env_config["render_mode"] = "human"
     demo_env = ParallelPettingZooEnv(GridWorldEnv(**env_config))
 
     reward, steps, num_breaks = test_one_episode(demo_env, tester)
     demo_env.close()
 
     pretty_print(f"Metrics for Demo Episode", reward, steps, num_breaks)
-    print("Running 30 more test episodes...")
 
-    total_reward, total_steps, total_breaks = 0, 0, 0
-    num_test_episodes = 30
-    for i in range(num_test_episodes):
-        print(f"\r{i}/{num_test_episodes}", end="")
-        reward, steps, num_breaks = test_one_episode(game_env, tester)
-        total_reward += reward
-        total_steps += steps
-        total_breaks += num_breaks
-    print("")
+    num_episodes = args.num_test_episodes
+    if num_episodes > 1:
+        print(f"Running {num_episodes} more test episodes...")
+        env_config["render_mode"] = "rgb_array"
+        game_env = ParallelPettingZooEnv(GridWorldEnv(**env_config))
 
-    avg_reward = round(total_reward / num_test_episodes, 2)
-    avg_steps = round(total_steps / num_test_episodes, 2)
-    avg_breaks = round(total_breaks / num_test_episodes, 2)
+        total_reward, total_steps, total_breaks = 0, 0, 0
+        num_episodes = args.num_test_episodes
+        for i in range(num_episodes):
+            print(f"\r{i}/{num_episodes}", end="")
+            reward, steps, num_breaks = test_one_episode(game_env, tester)
+            total_reward += reward
+            total_steps += steps
+            total_breaks += num_breaks
+        print("")
 
-    game_env.close()
+        avg_reward = round(total_reward / num_episodes, 2)
+        avg_steps = round(total_steps / num_episodes, 2)
+        avg_breaks = round(total_breaks / num_episodes, 2)
 
-    pretty_print(f"Averages Over {num_test_episodes} Test Episodes", avg_reward, avg_steps, avg_breaks)
+        game_env.close()
+
+        pretty_print(f"Averages Over {num_episodes} Test Episodes", avg_reward, avg_steps, avg_breaks)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -163,7 +165,7 @@ def main():
         config = yaml.safe_load(f)
 
     env_config = dict(
-        render_mode="rgb_array",
+        render_mode="rgb_array" if not args.test else "human",
         rw_scheme=config['reward_scheme'],
         seed=args.seed,
         **config['environment']
@@ -172,7 +174,7 @@ def main():
     # for rllib
     register_env("grid_world", lambda cfg: ParallelPettingZooEnv(GridWorldEnv(**cfg)))
 
-    print("Testing Parameters:" if args.test else "Training Parameters:")
+    print("Training Parameters:" if not args.test else "Testing Parameters:")
     print("-"*50)
     print(f"Using device: {args.device}")
     print(f"Seed: {args.seed}")
